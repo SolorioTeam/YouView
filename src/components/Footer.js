18c044a7cc228a578c400c48096b55d34a297320
Jason Solorio <jasonsoloriom@gmail.com>
2018-11-09 19:32:01 -0700

    social media buttons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,14 +4,14 @@ class Footer extends Component {
   render(){
     return (
       <footer>
-          <button className="backToTop"></button>
-          <p className="copyrights"></p>
-          <a className="companyLink"></a>
+          <button className="backToTop">Back to top</button>
+          <p className="copyrights">&copy; 2018 YouView</p>
+          <a className="companyLink">About Company</a>
           <span>
-              <button></button>
-              <button></button>
-              <button></button>
-              <button></button>
+              <span><i className="fab fa-facebook-f"></i>#4267b2</span>
+              <span><i className="fab fa-twitter"></i>#1da1f2</span>
+              <span><i className="fab fa-linkedin-in"></i>#0077B5</span>
+              <span><i className="fab fa-google-plus-g"></i>#db4437</span>
           </span>
       </footer>
     );
