2262e29658872cdbe579cfc2091028fdbb91c321
rsol7 <rsolm72@gmail.com>
2018-11-14 23:12:27 -0700

    Shallow testing sign up component

---
+++ src/containers/SignUp.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class SignUp extends Component{
+    render(){
+        return(
+            <div>
+                <form>
+                    <button> Submit </button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignUp;
