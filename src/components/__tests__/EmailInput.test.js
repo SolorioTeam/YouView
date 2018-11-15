481de2e6bc4ac12904a834746f3f548bb5fd414d
Jason Solorio <jasonsoloriom@gmail.com>
2018-11-15 02:34:06 -0700

    bad email

--- src/components/__tests__/EmailInput.test.js
+++ src/components/__tests__/EmailInput.test.js
@@ -40,6 +40,7 @@ describe("Email Input", () => {
 
     it("checks for the wrong value on the input", () => {
         let email_input = mounted_wrapper.find('input');
-        email_input.simulate('change', {target: {value: 'a@.com'}});
+        email_input.simulate('change', {target: {value: 'as@.com'}});
     });
+
 });
