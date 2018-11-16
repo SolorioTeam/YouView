7516a8a5f8316dfe452eed71edf5266be83f5fad
Jason Solorio <jasonsoloriom@gmail.com>
2018-11-16 13:53:56 -0700

    Bettering the regex to require two characters after .

--- src/components/EmailInput.js
+++ src/components/EmailInput.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-const emailRegex = /\S+@\S+\.[a-zA-Z]/;
+const emailRegex = /\S+@\S+\.[a-zA-Z]{2}/;
 
 class EmailInput extends Component{
     constructor(props){
@@ -13,37 +13,20 @@ class EmailInput extends Component{
 
     changeInput(e){
         this.setState({email: e.target.value},
-        () => {
-            if(this.state.email.match(emailRegex) )
-            this.setState(
-                {validEmail: true},
-                () => console.log(this.state.validEmail)
-                // () => this.props.emailValue(this.state.email) 
-                );
-            else 
-            this.setState(
-                {validEmail: false},
-                () => console.log(this.state.validEmail)
-
-                // () => this.props.emailValue("") 
-                );
-        }    
+            () => {
+                if(this.state.email.match(emailRegex) )
+                    this.setState(
+                        {validEmail: true},
+                        // () => this.props.emailValue(this.state.email) 
+                    );
+                else 
+                    this.setState(
+                        {validEmail: false},
+                        // () => this.props.emailValue("") 
+                    );
+            }    
         );
-    }
-
-    // validEmailHandler = () => {
-
-    //     if(this.state.email.match(emailRegex) )
-    //         this.setState(
-    //             {validEmail: true},
-    //             // () => this.props.emailValue(this.state.email) 
-    //             );
-    //     else 
-    //         this.setState(
-    //             {validEmail: false},
-    //             // () => this.props.emailValue("") 
-    //             );
-    // }
+    };
 
     render(){
         return(
@@ -53,8 +36,17 @@ class EmailInput extends Component{
                     type="email" 
                     id="Email" 
                     value={this.state.email}
+                    placeholder="Email"
                     onChange={(e) => this.changeInput(e)}
+                    className={ "form-control " + ( this.state.validEmail != null ?
+                                                  ( this.state.validEmail ? "is-valid" : "is-invalid") : "") }
                 />
+                {
+                    this.state.validEmail ? "" :
+                        <div className="invalid-feedback">
+                            Please provide a valid email.
+                        </div>
+                }
             </div>
         )
     }
